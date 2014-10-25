c74c86f320260493fc2c4348f234aa58f954238c
cassiocosta <cassio.hc@gmail.com>
2014-10-25 12:41:21 -0200

    commit project

---
+++ AppEstrelinha/Models/Alunos.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Linq.Mapping;
+
+namespace AppEstrelinha.Models
+{
+    [Table]//Tabela do Banco (Alunos).
+    public class Alunos
+    {
+        #region Atributos
+        
+        private int _idAluno;
+        private string _nome;
+        private int _idDisc;
+        private int _nEstrelas;
+
+        #endregion
+
+        #region Propriedades
+
+        [Column(IsPrimaryKey = true, IsDbGenerated = true)]//Chave primária.
+        public int IdAluno
+        {
+            get { return _idAluno; }
+            set { _idAluno = value; }
+        }
+
+        [Column(CanBeNull = false)]//Não recebe null.
+        public string Nome
+        {
+            get { return _nome; }
+            set { _nome = value; }
+        }
+
+        [Column(CanBeNull = false)]//Não recebe null.
+        public int IdDisc
+        {
+            get { return _idDisc; }
+            set { _idDisc = value; }
+        }
+
+        [Column(CanBeNull = false)]
+        public int NEstrelas
+        {
+            get { return _nEstrelas; }
+            set { _nEstrelas = value; }
+        }
+        
+        #endregion
+        
+    }
+}
---
+++ AppEstrelinha/Models/AlunosDB.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AppEstrelinha.Models
+{
+    class AlunosDB
+    {
+        public static DataBase getDataBase()
+        {
+            DataBase db = new DataBase();
+            if (db.DatabaseExists() == false)
+            {
+                db.CreateDatabase();
+            }
+            return db;
+        }
+
+        public static string save(Alunos pAlu)
+        {
+            if (pAlu.Nome != "")
+            {
+                DataBase db = getDataBase();
+                db.Alunos.InsertOnSubmit(pAlu);
+                db.SubmitChanges();
+            }
+            else
+            {
+                return "Preencha o campo Nome.";
+            }
+            return pAlu.Nome + " cadastrado com Sucesso";
+        }
+
+        
+
+        public static string update(Alunos pAlu) 
+        {
+            DataBase db = getDataBase();
+            Alunos update = (from d in db.Alunos
+                             where d.IdAluno == pAlu.IdAluno && d.IdDisc == pAlu.IdDisc
+                             select d).First();
+            update.Nome = pAlu.Nome;
+            db.SubmitChanges();
+
+            return "Update feito com Sucesso!";
+        }
+
+        public static string updateEstrelas(Alunos pAlu)
+        {
+            DataBase db = getDataBase();
+            Alunos update = (from d in db.Alunos
+                             where d.IdAluno == pAlu.IdAluno && d.IdDisc == pAlu.IdDisc
+                             select d).First();
+            update.NEstrelas = pAlu.NEstrelas + update.NEstrelas;
+            db.SubmitChanges();
+
+            return "Update feito com Sucesso!";
+        }
+
+        public static List<Alunos> getAlunos()
+        {
+            DataBase db = getDataBase();
+            var query = from d in db.Alunos orderby d.Nome select d;
+            List<Alunos> alunos = new List<Alunos>(query.AsEnumerable());
+            return alunos;
+        }
+
+        public static List<Alunos> getAlunos(int pDisc)
+        {
+            DataBase db = getDataBase();
+            var query = from d in db.Alunos
+                        where d.IdDisc.Equals(pDisc)
+                        orderby d.Nome
+                        select d;
+            List<Alunos> alunos = new List<Alunos>(query.AsEnumerable());
+            return alunos;
+        }
+
+        public static void delet(Models.Alunos pAluno) 
+        {
+            DataBase db = getDataBase();
+            var query = from a in db.Alunos where a.IdAluno == pAluno.IdAluno select a;
+            db.Alunos.DeleteOnSubmit(query.ToList()[0]);
+            db.SubmitChanges();
+            
+        }
+
+        public static void deleteAll()
+        {
+            DataBase db = getDataBase();
+            var query = from a in db.Alunos select a;
+            db.Alunos.DeleteAllOnSubmit(query);
+            db.SubmitChanges();
+        }
+
+    }
+}
